a105768b3cd05433829431ed36ca25dd9673770e
davidcox95 <davidcox95@comcast.net>
2014-04-17 21:19:48 -0400

    removed old server commented.

--- server.js
+++ server.js
@@ -13,19 +13,4 @@ app.use(function(req, res) {
 
 app.listen(8080);
 console.log('Listening on port 8080...');
-/*
-var express = require('express');
-var fs = require('fs');
-var htmlfile = 'index.html';
-var app = express();
 
-app.get('/', function(request, response) {
-    var html = fs.readFileSync(htmlfile).toString();
-    response.send(html);
-});
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-    console.log('Listening on ' + port);
-});
-*/
